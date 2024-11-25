98ad879f882eb7f6f359e359e9aca2688594ed2b
Danis <danisharisov@gmail.com>
2024-11-25 14:50:40 +0300

    Rabbit fixed

--- src/main/java/com/example/Tbank_fj_2024_COURSE_PROJECT/config/RabbitMQConfig.java
+++ src/main/java/com/example/Tbank_fj_2024_COURSE_PROJECT/config/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.example.Tbank_fj_2024_COURSE_PROJECT.config;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +14,18 @@ public class RabbitMQConfig {
 
     @Bean
     public Queue telegramUpdatesQueue() {
-        return new Queue(QUEUE_NAME, true);
+        return new Queue(QUEUE_NAME, true); // durable = true
     }
 
     @Bean
     public Jackson2JsonMessageConverter jacksonConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jacksonConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jacksonConverter);
+        return template;
     }
 }
