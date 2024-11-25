33a956363d7adf2b082a1b94a7fd50df99ae6d2d
Danis <danisharisov@gmail.com>
2024-11-25 13:34:33 +0300

    WeeklyReminder added

--- src/main/java/com/example/Tbank_fj_2024_COURSE_PROJECT/TbankFj2024CourseProjectApplication.java
+++ src/main/java/com/example/Tbank_fj_2024_COURSE_PROJECT/TbankFj2024CourseProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.Tbank_fj_2024_COURSE_PROJECT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TbankFj2024CourseProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TbankFj2024CourseProjectApplication.class, args);
--- src/main/java/com/example/Tbank_fj_2024_COURSE_PROJECT/services/AppUserService.java
+++ src/main/java/com/example/Tbank_fj_2024_COURSE_PROJECT/services/AppUserService.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AppUserService {
     private static final Logger logger = LoggerFactory.getLogger(AppUserService.class);
@@ -32,5 +34,11 @@ public class AppUserService {
     @Transactional
     public void saveUser(AppUser user) {
         appUserRepository.save(user);
+    }
+
+    //Возвращает всех пользователей
+    @Transactional
+    public List<AppUser> findAllUsers() {
+        return appUserRepository.findAll();
     }
 }
